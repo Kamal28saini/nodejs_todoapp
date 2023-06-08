2de0bad38c18b6d0ad960f6dcac09048082f5902
Kamal28saini <97048337+Kamal28saini@users.noreply.github.com>
2023-06-08 18:43:22 +0530

    fixed enine

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ export const connectDB = ()=>{
 // MongoDB connection 
 mongoose.connect(process.env.MONGO_URL, {
 dbName: "backendapi",
-}).then(() => console.log("Database is connected"))
+}).then((c) => console.log(`Database is connected with ${c.connection.host}`))
 .catch((e) => console.log(e));
 };
