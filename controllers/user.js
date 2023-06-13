bd8f6c1ba429ed2e19b1ad1118bf7cc5f745d78c
Kamal28saini <97048337+Kamal28saini@users.noreply.github.com>
2023-06-13 15:00:32 +0530

    ./controller/user.js file is updated

--- controllers/user.js
+++ controllers/user.js
@@ -28,7 +28,7 @@ export const register = async(req, res,next) => {
 
 
 
-export const login = async(req,res) =>{
+export const login = async(req,res,next) =>{
     try {
         const {email,password} = req.body;
 
