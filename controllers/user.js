e72abc022a2f0459785b0a9cfe94b22aca31060c
Kamal28saini <97048337+Kamal28saini@users.noreply.github.com>
2023-06-13 14:48:02 +0530

    ./controller/user.js file is updated

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ import { sendCookie } from "../utils/features.js";
 
 
 // register new user
-export const register = async(req, res) => {
+export const register = async(req, res,next) => {
     try {
         const {name,email,password} = req.body;
 
